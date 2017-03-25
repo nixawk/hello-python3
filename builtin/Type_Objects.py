8b9e472c13e2bcee1f4499523227ee7723e2b6a5
nixawk <hap.ddup@gmail.com>
2017-03-25 10:22:00 -0500

    Type Objects

---
+++ builtin/Type_Objects.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+"""
+Type objects represent the various object types. An object's type is accessed
+by the built-in function type(). There are no special operations on types.
+The standard module types defines names for all standard built-in types.
+"""
+
+
+class TypeObject(object):
+    pass
+
+
+print(type(TypeObject()))
+print(type(int()))
