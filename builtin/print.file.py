91df38d5b9c016b755c2a52568708bc581f494d8
nixawk <hap.ddup@gmail.com>
2017-04-01 05:17:08 -0500

    print function

---
+++ builtin/print.file.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+with open('/tmp/testfile', 'w') as f:
+    print('helloworld\n', file=f)
---
+++ builtin/print.sep.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+print(value, ..., sep=' ', end='\n', file=sys.stdout, flush=False)
+
+Prints the values to a stream, or to sys.stdout by default.
+Optional keyword arguments:
+file:  a file-like object (stream); defaults to the current sys.stdout.
+sep:   string inserted between values, default a space.
+end:   string appended after the last value, default a newline.
+flush: whether to forcibly flush the stream.
+Type:      builtin_function_or_method
+"""
+
+print(1111, 2222, 3333, 4444, sep='-')
