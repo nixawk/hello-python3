f2ba91ecfda02e806a2b8178d8363beb3141d444
nixawk <hap.ddup@gmail.com>
2018-04-09 22:03:16 -0500

    pep 8 Introduction

---
+++ PEP/pep-008-01-Introduction.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# Introduction
+# This document gives coding conventions for the Python code comprising
+# the standard library in the main Python distribution. Please see the
+# companion information PEP describing style guidelines for the C code
+# in the C implementation of Python.
+
+# This document and PEP 257 (Docstring Conventions) were adapted from
+# Guido's original Python Style Guide essay, with some additional from
+# Barry's style guide.
+
+# This style guide evolves over time as additional conventions are identified
+# and past conventions are rendered absolete by changes in the language itself.
+
+# Many projects have their own coding style guidelines. In the event of
+# any conflicts, such project-specific guides take precedence for taht project.
+
+# https://www.python.org/dev/peps/pep-0008/
+# https://www.python.org/dev/peps/pep-0257
+# https://barry.warsaw.us/software/STYLEGUIDE.txt
