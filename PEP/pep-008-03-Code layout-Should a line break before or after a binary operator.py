3cc586cb3dc05f29fc7bbcbe7a17c1305e7f74e5
nixawk <hap.ddup@gmail.com>
2018-04-10 09:34:19 -0500

    pep 484 - Should a line break before or after a binary operator

---
+++ PEP/pep-008-03-Code layout-Should a line break before or after a binary operator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+def long_function_name(
+    var_aaaaaaaa, var_bbbbbbbb,
+    var_cccccccc, var_dddddddd,
+    var_eeeeeeee, var_ffffffff):
+    '''docstring'''
+
+    # easy to match operators with operands
+    val = (var_aaaaaaaa
+           + var_bbbbbbbb
+           + var_cccccccc
+           + var_dddddddd
+           + var_eeeeeeee
+           + var_ffffffff)
+
+    return val
+
+
+# https://www.python.org/dev/peps/pep-0008/#should-a-line-break-before-or-after-a-binary-operator
