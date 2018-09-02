65388ef6bc89bb348a14aa2b23da4af7940c9902
Nixawk <hap.ddup@gmail.com>
2018-09-02 18:12:06 +0800

    threading.Thread

--- threading/threading.Thread.py
+++ threading/threading.Thread.py
@@ -1,17 +1,54 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+"""
+class threading.Thread(group=None, target=None, name=None, args=(), kwargs={}, *, daemon=None)
+
+
+threading.Thread.start
+threading.Thread.run
+threading.Thread.join
+threading.Thread.name
+threading.Thread.getName
+threading.Thread.setName
+threading.Thread.ident
+threading.Thread.is_alive
+threading.Thread.daemon
+threading.Thread.isDaemon
+threading.Thread.setDaemon
+"""
+
+from __future__ import print_function
+from __future__ import unicode_literals
+
 import threading
 
 
-def worker():
-    """thread worker function"""
-    print('Worker')
+class MyThread(threading.Thread):
+
+    def __init__(self, *args, **kwargs):
+        super(MyThread, self).__init__(*args, **kwargs)
+
+    def run(self):
+        print("called by threading.Thread.start()")
+
+        # def start(self):
+        #     _start_new_thread(self._bootstrap, ())
+
+        # def _bootstrap(self):
+        #     self._bootstrap_inner()
+
+        # def _bootstrap_inner(self):
+        #     self.run()
 
 
 if __name__ == '__main__':
-    threads = []
-    for i in range(5):
-        t = threading.Thread(target=worker)
-        threads.append(t)
-        t.start()
+    mythread = MyThread()
+    mythread.start()
+    mythread.join()
+
+# References
+# https://docs.python.org/3/library/threading.html#threading.Thread
+# https://github.com/python/cpython/blob/master/Lib/threading.py#L854
+# https://stackoverflow.com/questions/23224784/start-vs-run-for-threads-in-python
+
