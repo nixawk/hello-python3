77f5e53aba38fa9a16b3e54213d1e5896e92147c
Nixawk <hap.ddup@gmail.com>
2019-06-26 10:39:24 +0800

    gather

--- asyncio/asyncio-gather.py
+++ asyncio/asyncio-gather.py
@@ -5,31 +5,49 @@
 # They run for ten minutes, during which the first coroutine is scheduled
 # to run every second, while the second is scheduled to run every minute.
 
-
+import random
 import asyncio
 
 
-async def print_every_second():
-    """Print seconds"""
-    while True:
-        for i in range(60):
-            print(i, 's')
-            await asyncio.sleep(i)
-
-
-async def print_every_minute():
-    """Print minute"""
-    for i in range(1, 10):
-        await asyncio.sleep(60)
-        print(i, 'minute')
-
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(
-    asyncio.gather(print_every_second(),
-                   print_every_minute())
+COLORS = (
+    "\033[0;0m",  # 'End'
+    "\033[0;30m", # 'Black'
+    "\033[0;31m", # 'Red'
+    "\033[0;32m", # 'Green' 
+    "\033[0;33m", # 'Brown' 
+    "\033[0;34m", # 'Blue'
+    "\033[0;36m", # 'Cyan'
+    "\033[0;35m", # 'Purple'
+    "\033[0;37m", # 'Light Gray'
+    "\033[1;30m", # 'Dark Gray'
+    "\033[1;31m", # 'Light Red'
+    "\033[1;32m", # 'Light Green'
+    "\033[1;33m", # 'Yellow'
+    "\033[1;34m", # 'Light Blue'
+    "\033[1;35m", # 'Light Purple'
+    "\033[1;36m", # 'Light Cyan'
+    "\033[1;37m", # 'White'
 )
-loop.close()
 
 
+async def makerandom() -> int:
+    coidx = random.randint(1, len(COLORS) - 1)
+    color = COLORS[coidx]
+
+    print(color + f"ColorIndex: {coidx:02d}, Message: This is a color string" + COLORS[0])
+    await asyncio.sleep(coidx)
+    return coidx
+
+
+async def main():
+    res = await asyncio.gather(*(makerandom() for i in range(20)))
+    return res
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
+
+# references
 # http://asyncio.readthedocs.io/en/latest/hello_clock.html
+# http://www.tldp.org/HOWTO/Bash-Prompt-HOWTO/x329.html
