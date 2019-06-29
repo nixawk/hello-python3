f60e118996905b4318269ace5246fed31e7b11f6
Nixawk <hap.ddup@gmail.com>
2019-06-29 10:11:27 +0800

    add __future__.print_function

---
+++ __future__/__future__.print_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+
+if __name__ == '__main__':
+    print("__future__.print_function")
+
+
+# references
+# https://github.com/python/cpython/blob/master/Lib/__future__.py
